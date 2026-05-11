e7b95999aac526543211b4f405ff144cee830c61
Tyler <84472877+Tylersed@users.noreply.github.com>
2026-05-11 10:41:07 -0400

    Add startDate and endDate fields to notifications

--- notifications.js
+++ notifications.js
@@ -13,7 +13,9 @@ let NOTIFICATIONS = [
     "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     "secondaryLinkText": "Directions",
     "secondaryUrl": "https://maps.apple.com/?q=2325+Lakeview+Parkway+Suite+375+Alpharetta+GA+30009",
-    "expires": ""
+    "expires": "",
+    "startDate": "",
+    "endDate": ""
   },
   {
     "active": true,
@@ -22,7 +24,9 @@ let NOTIFICATIONS = [
     "message": "ROAR stands for Refined Operations, Advisor Resources. Use this page as your starting point for tools, updates, and support.",
     "linkText": "Open Advisor Resource Center",
     "url": "https://tylersed.github.io/PTC-notes/",
-    "expires": ""
+    "expires": "",
+    "startDate": "",
+    "endDate": ""
   },
   {
     "active": true,
@@ -31,6 +35,8 @@ let NOTIFICATIONS = [
     "message": "Use Support Concierge to choose the right contact before sending a request.",
     "linkText": "Open Support Concierge",
     "url": "https://outlook.cloud.microsoft/mail/deeplink/compose?to=amy%40peachtreetc.com&subject=Advisor+General+Support+Request&body=Hi%2C%0A%0AI+need+help+with%3A%0A%0A",
-    "expires": ""
+    "expires": "",
+    "startDate": "",
+    "endDate": ""
   }
 ];
